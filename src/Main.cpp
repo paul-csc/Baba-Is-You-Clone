ac9d1bb49daa3f881e7e07d418d7d852d7515841
paul-csc <paulchuxin.liu@outlook.com>
2026-02-01 20:55:59 -0500

    Add depth

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,32 +1,78 @@
-#include <raylib.h>
+#include "raylib.h"
 #include <array>
 #include <cassert>
+#include <cstdint>
 #include <string>
 #include <vector>
 
-constexpr int TILE_SIZE = 48;
+constexpr float TILE_SIZE = 48;
 constexpr int LEVEL_WIDTH = 33;
 constexpr int LEVEL_HEIGHT = 18;
 constexpr int SCREEN_WIDTH = TILE_SIZE * LEVEL_WIDTH;
 constexpr int SCREEN_HEIGHT = TILE_SIZE * LEVEL_HEIGHT;
+
+constexpr size_t MAX_OBJECT_PER_TILE = 6;
+
+constexpr int NUM_LEVEL = 2;
 
 struct Vec2i {
     int x;
     int y;
 };
 
+enum class TileType { Empty, Wall, Player, Flag, Rock };
+
+class Tile {
+  public:
+    void Push(TileType type) {
+        if (m_numObjects >= MAX_OBJECT_PER_TILE) {
+            return;
+        }
+
+        m_objects[m_numObjects++] = type;
+    }
+
+    TileType Pop() {
+        assert(m_numObjects >= 1);
+        return m_objects[--m_numObjects];
+    }
+
+    bool IsEmpty() const { return m_numObjects == 0; }
+
+    bool Contains(TileType type) const {
+        for (int i = 0; i < m_numObjects; ++i) {
+            if (m_objects[i] == type) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // iteration (non-const)
+    auto begin() { return m_objects.begin(); }
+    auto end() { return m_objects.begin() + m_numObjects; }
+
+    // iteration (const)
+    auto begin() const { return m_objects.begin(); }
+    auto end() const { return m_objects.begin() + m_numObjects; }
+
+  private:
+    std::array<TileType, MAX_OBJECT_PER_TILE> m_objects;
+    int m_numObjects = 0;
+};
+
 // ' ' = empty
 // '#' = wall
-// 'B' = rock
+// 'R' = rock
 // 'F' = flag
 // '@' = player start
 
 // clang-format off
-static const std::vector<std::array<std::array<char, LEVEL_WIDTH + 1>, LEVEL_HEIGHT>> Levels{
+static const char Levels[NUM_LEVEL][LEVEL_HEIGHT][LEVEL_WIDTH + 1] = {
 {
     "#################################",
     "#       @                       #",
-    "#           B B                 #",
+    "#           R R                 #",
     "#                               #",
     "#      F          ##            #",
     "#                               #",
@@ -45,12 +91,12 @@ static const std::vector<std::array<std::array<char, LEVEL_WIDTH + 1>, LEVEL_HEI
 }, {
     "#################################",
     "#       @                       #",
-    "#           B B                 #",
+    "#           RRRRRRRR            #",
     "#                               #",
     "#      F          ##            #",
     "#                               #",
     "#                               #",
-    "#    BBBBBBBBBBBBBBBBBBB        #",
+    "#    RRRRRRRRRRRRRRRRRRR        #",
     "#                               #",
     "#                               #",
     "#                               #",
@@ -65,8 +111,6 @@ static const std::vector<std::array<std::array<char, LEVEL_WIDTH + 1>, LEVEL_HEI
 };
 // clang-format on
 
-enum Tile { EMPTY, WALL };
-
 class Game {
   public:
     Game() {
@@ -78,22 +122,24 @@ class Game {
     ~Game() { CloseWindow(); }
 
     void Update() {
-        if (IsKeyPressed(KEY_W)) {
-            TryMove(0, -1);
-        } else if (IsKeyPressed(KEY_S)) {
-            TryMove(0, 1);
-        } else if (IsKeyPressed(KEY_A)) {
-            TryMove(-1, 0);
-        } else if (IsKeyPressed(KEY_D)) {
-            TryMove(1, 0);
+        if (!m_isWin) {
+            if (IsKeyPressed(KEY_W)) {
+                TryMove(0, -1);
+            } else if (IsKeyPressed(KEY_S)) {
+                TryMove(0, 1);
+            } else if (IsKeyPressed(KEY_A)) {
+                TryMove(-1, 0);
+            } else if (IsKeyPressed(KEY_D)) {
+                TryMove(1, 0);
+            }
         }
 
         if (IsKeyPressed(KEY_R)) {
-            LoadLevel(m_levelNum); // reset
-        } else if (IsKeyPressed(KEY_N) && m_levelNum < Levels.size()) {
-            LoadLevel(m_levelNum + 1);
+            LoadLevel(m_levelNum);
+        } else if (IsKeyPressed(KEY_N) && m_levelNum < NUM_LEVEL) {
+            LoadLevel(++m_levelNum);
         } else if (IsKeyPressed(KEY_P) && m_levelNum > 1) {
-            LoadLevel(m_levelNum - 1);
+            LoadLevel(--m_levelNum);
         }
     }
 
@@ -101,10 +147,32 @@ class Game {
         BeginDrawing();
         ClearBackground({ 20, 20, 20, 255 });
 
-        DrawGrid();
-        DrawRocks();
-        DrawFlags();
-        DrawPlayer();
+        for (int y = 0; y < LEVEL_HEIGHT; ++y) {
+            for (int x = 0; x < LEVEL_WIDTH; ++x) {
+                Rectangle r = { x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE };
+
+                DrawRectangleRec(r, { 50, 50, 50, 255 });
+                DrawRectangleLines((int)r.x, (int)r.y, (int)r.width, (int)r.height, { 30, 30, 30, 255 });
+
+                for (const auto object : m_tiles[y][x]) {
+                    if (object == TileType::Wall) {
+                        DrawRectangleRec(r, DARKGRAY);
+                    } else if (object == TileType::Rock) {
+                        DrawRectangleRounded({ r.x + 4.0f, r.y + 4.0f, TILE_SIZE - 8, TILE_SIZE - 8 }, 0.3f,
+                            6, { 150, 100, 60, 255 });
+                    } else if (object == TileType::Flag) {
+                        DrawRectangle(r.x + 15, r.y + 5, TILE_SIZE - 42, TILE_SIZE - 10, YELLOW);
+                        DrawRectangle(r.x + 21, r.y + 5, 17, 16, YELLOW);
+                    } else if (object == TileType::Player) {
+                        DrawRectangleRec({ r.x + 6, r.y + 6, TILE_SIZE - 12, TILE_SIZE - 12 }, BLUE);
+
+                        // eyes
+                        DrawRectangleRec({ r.x + 13, r.y + 15, 7, 7 }, BLACK);
+                        DrawRectangleRec({ r.x + TILE_SIZE - 20, r.y + 15, 7, 7 }, BLACK);
+                    }
+                }
+            }
+        }
 
         if (m_isWin) {
             DrawText("You Win!", 50, 50, 50, GREEN);
@@ -116,159 +184,92 @@ class Game {
   private:
     void LoadLevel(int number) {
         number--;
+        assert(number >= 0 && number < NUM_LEVEL);
 
-        m_rocks.clear();
-        m_flags.clear();
-        m_player = { 0, 0 };
         m_isWin = false;
-
-        bool isTherePlayer = false;
-        bool isThereFlag = false;
+        bool hasPlayer = false;
+        bool hasFlag = false;
 
         for (int y = 0; y < LEVEL_HEIGHT; ++y) {
             for (int x = 0; x < LEVEL_WIDTH; ++x) {
-                m_level[y][x] = EMPTY;
+                m_tiles[y][x] = Tile{};
 
                 switch (Levels[number][y][x]) {
-                    case 'B': m_rocks.emplace_back(x, y); break;
+                    case '#': m_tiles[y][x].Push(TileType::Wall); break;
+                    case 'R': m_tiles[y][x].Push(TileType::Rock); break;
                     case 'F':
-                        m_flags.emplace_back(x, y);
-                        isThereFlag = true;
+                        m_tiles[y][x].Push(TileType::Flag);
+                        hasFlag = true;
                         break;
                     case '@':
+                        m_tiles[y][x].Push(TileType::Player);
                         m_player = { x, y };
-                        isTherePlayer = true;
+                        hasPlayer = true;
                         break;
-                    case '#': m_level[y][x] = WALL; break;
                     case ' ': break;
-                    default: assert(0);
+                    default: assert(false);
                 }
             }
         }
 
-        assert(isThereFlag);
-        assert(isTherePlayer);
-    }
-
-    int RockIndexAt(int x, int y) const {
-        for (int i = 0; i < m_rocks.size(); ++i) {
-            if (m_rocks[i].x == x && m_rocks[i].y == y) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    int FlagIndexAt(int x, int y) const {
-        for (int i = 0; i < m_flags.size(); ++i) {
-            if (m_flags[i].x == x && m_flags[i].y == y) {
-                return i;
-            }
-        }
-        return -1;
+        assert(hasPlayer);
+        assert(hasFlag);
     }
 
     static bool InBounds(int x, int y) { return x >= 0 && x < LEVEL_WIDTH && y >= 0 && y < LEVEL_HEIGHT; }
 
     void TryMove(int dx, int dy) {
-        int nx = m_player.x + dx;
-        int ny = m_player.y + dy;
+        const int nx = m_player.x + dx;
+        const int ny = m_player.y + dy;
 
-        if (!InBounds(nx, ny)) { // off-screen
-            return;
-        }
-        if (m_level[ny][nx] == WALL) {
-            return; // can't move into wall
-        }
-        if (FlagIndexAt(nx, ny) != -1) {
-            m_isWin = true;
-        }
-
-        int rockIndex = RockIndexAt(nx, ny);
-        if (rockIndex == -1) {
-            // empty -> move player
-            m_player.x = nx;
-            m_player.y = ny;
+        if (!InBounds(nx, ny) || m_tiles[ny][nx].Contains(TileType::Wall)) {
             return;
         }
 
-        int bx = nx + dx;
-        int by = ny + dy;
+        // scan forward until non-rock
+        int cx = nx;
+        int cy = ny;
+        while (InBounds(cx, cy) && m_tiles[cy][cx].Contains(TileType::Rock)) {
+            cx += dx;
+            cy += dy;
+        }
 
-        while (InBounds(bx, by)) {
-            if (m_level[by][bx] == WALL) {
-                return;
+        if (!InBounds(cx, cy) || m_tiles[cy][cx].Contains(TileType::Wall)) {
+            return;
+        }
+
+        // if destination cell already has a rock (shouldn't happen since we scanned), block
+        // but scanning should stop at first non-rock, so it's safe.
+
+        // shift rocks forward (back-to-front)
+        while (cx != nx || cy != ny) {
+            const int px = cx - dx;
+            const int py = cy - dy;
+
+            // if there is a rock at source, move it to destination
+            if (m_tiles[py][px].Contains(TileType::Rock)) {
+                m_tiles[cy][cx].Push(TileType::Rock);
+                m_tiles[py][px].Pop();
             }
+
+            cx = px;
+            cy = py;
         }
 
-        if (!InBounds(bx, by)) { // off-screen
-            return;
-        }
-
-        if (m_level[by][bx] == WALL || RockIndexAt(bx, by) != -1) {
-            return;
-        }
-
-        // push rock and move player
-        if (rockIndex >= 0 && (size_t)rockIndex < m_rocks.size()) {
-            m_rocks[rockIndex].x = bx;
-            m_rocks[rockIndex].y = by;
-        }
-
+        // move player
+        m_tiles[m_player.y][m_player.x].Pop();
+        m_tiles[ny][nx].Push(TileType::Player);
         m_player.x = nx;
         m_player.y = ny;
-    }
 
-    void DrawGrid() const {
-        for (int y = 0; y < LEVEL_HEIGHT; ++y) {
-            for (int x = 0; x < LEVEL_WIDTH; ++x) {
-                Rectangle r = { (float)(x * TILE_SIZE), (float)(y * TILE_SIZE), (float)TILE_SIZE,
-                    (float)TILE_SIZE };
-
-                Color cellColor = m_level[y][x] == WALL ? DARKGRAY : Color{ 50, 50, 50, 255 };
-                DrawRectangleRec(r, cellColor);
-
-                // grid lines (cast to ints for the lines version)
-                DrawRectangleLines((int)r.x, (int)r.y, (int)r.width, (int)r.height, Color{ 30, 30, 30, 255 });
-            }
+        if (m_tiles[ny][nx].Contains(TileType::Flag)) {
+            m_isWin = true;
         }
-    }
-
-    void DrawRocks() const {
-        for (const auto& b : m_rocks) {
-            int px = b.x * TILE_SIZE;
-            int py = b.y * TILE_SIZE;
-
-            Rectangle rect{ px + 4.0f, py + 4.0f, TILE_SIZE - 8, TILE_SIZE - 8 };
-            DrawRectangleRounded(rect, 0.3f, 6, Color{ 150, 100, 60, 255 });
-        }
-    }
-
-    void DrawFlags() const {
-        for (const auto& flag : m_flags) {
-            int px = flag.x * TILE_SIZE;
-            int py = flag.y * TILE_SIZE;
-
-            DrawRectangle(px + 15, py + 5, TILE_SIZE - 42, TILE_SIZE - 10, YELLOW);
-            DrawRectangle(px + 21, py + 5, 17, 16, YELLOW);
-        }
-    }
-
-    void DrawPlayer() const {
-        int px = m_player.x * TILE_SIZE;
-        int py = m_player.y * TILE_SIZE;
-        DrawRectangle(px + 6, py + 6, TILE_SIZE - 12, TILE_SIZE - 12, BLUE);
-
-        // eyes
-        DrawRectangle(px + 13, py + 15, 7, 7, BLACK);
-        DrawRectangle(px + TILE_SIZE - 20, py + 15, 7, 7, BLACK);
     }
 
   private:
-    std::array<std::array<Tile, LEVEL_WIDTH>, LEVEL_HEIGHT> m_level;
-    Vec2i m_player;
-    std::vector<Vec2i> m_rocks;
-    std::vector<Vec2i> m_flags;
+    std::array<std::array<Tile, LEVEL_WIDTH>, LEVEL_HEIGHT> m_tiles;
+    Vec2i m_player; // index into m_tiles
 
     std::vector<int> m_undoStack;
 
